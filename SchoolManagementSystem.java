716b4fce1b91c55a5b0126cdfa104ca2558d9a03
Nihal Sayyad <117541272+Nihal54@users.noreply.github.com>
2024-03-06 13:45:06 +0530

    Add files via upload

---
+++ SchoolManagementSystem.java
@@ -0,0 +1,191 @@
+import java.util.*;
+
+class Teacher {
+
+    private int id;
+    private String name;
+    private int salary;
+    private int salaryEarned;
+
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.salaryEarned = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void receiveSalary(int salary) {
+        salaryEarned += salary;
+        School.updateTotalMoneySpent(salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Name of the Teacher: " + name
+                + " Total salary earned so far $"
+                + salaryEarned;
+    }
+}
+
+class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student(int id, String name, int grade) {
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void payFees(int fees) {
+        feesPaid += fees;
+        School.updateTotalMoneyEarned(feesPaid);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getFeesPaid() {
+        return feesPaid;
+    }
+
+    public int getFeesTotal() {
+        return feesTotal;
+    }
+
+    public int getRemainingFees() {
+        return feesTotal - feesPaid;
+    }
+
+    @Override
+    public String toString() {
+        return "Student's name :" + name +
+                " Total fees paid so far $" + feesPaid;
+    }
+}
+
+class School {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private static int totalMoneyEarned;
+    private static int totalMoneySpent;
+
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public static void updateTotalMoneyEarned(int MoneyEarned) {
+        totalMoneyEarned += MoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public static void updateTotalMoneySpent(int moneySpent) {
+        totalMoneyEarned -= moneySpent;
+    }
+}
+
+public class SchoolManagementSystem {
+    public static void main(String[] args) {
+        Teacher lizzy = new Teacher(1, "Lizzy", 500);
+        Teacher mellisa = new Teacher(2, "Mellisa", 700);
+        Teacher vanderhorn = new Teacher(3, "Vanderhorn", 600);
+
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(lizzy);
+        teacherList.add(mellisa);
+        teacherList.add(vanderhorn);
+
+        Student tamasha = new Student(1, "Tamasha", 4);
+        Student rakshith = new Student(2, "Rakshith Vasudev", 12);
+        Student rabbi = new Student(3, "Rabbi", 5);
+        List<Student> studentList = new ArrayList<>();
+
+        studentList.add(tamasha);
+        studentList.add(rabbi);
+        studentList.add(rakshith);
+
+        School ghs = new School(teacherList, studentList);
+
+        Teacher megan = new Teacher(6, "Megan", 900);
+        ghs.addTeacher(megan);
+
+        tamasha.payFees(5000);
+        rakshith.payFees(6000);
+        System.out.println("GHS has earned $" + ghs.getTotalMoneyEarned());
+
+        System.out.println("------Making SCHOOL PAY SALARY----");
+        lizzy.receiveSalary(lizzy.getSalary());
+        System.out.println("GHS has spent for salary to " + lizzy.getName()
+                + " and now has $" + ghs.getTotalMoneyEarned());
+
+        vanderhorn.receiveSalary(vanderhorn.getSalary());
+        System.out.println("GHS has spent for salary to " + vanderhorn.getName()
+                + " and now has $" + ghs.getTotalMoneyEarned());
+
+        System.out.println(rakshith);
+        mellisa.receiveSalary(mellisa.getSalary());
+        System.out.println(mellisa);
+    }
+}
